bb6c9042cca1056e98b6de1273d9fa8465e2d635
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-03-02 15:17:20 -0300

    organizando as pastas, crinado e colocando os exercicios na pasta javaScript

--- iniciante/javaScript/1041.js
+++ iniciante/javaScript/1041.js
@@ -6,11 +6,11 @@ let linhas = input.split(' ');
 const x = parseFloat(linhas.shift());
 const y = parseFloat(linhas.shift());
 
-if (x == 0 && y == 0) {
+if (x === 0 && y === 0) {
     console.log("Origem");
-} else if (x == 0) {
+} else if (x === 0) {
     console.log("Eixo Y");
-} else if (y == 0) {
+} else if (y === 0) {
     console.log("Eixo X");
 } else if (y > 0 && x > 0) {
     console.log("Q1");
